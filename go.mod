5522192562ed5a8ee0d81839639d7fab17cff380
Pramesti Hatta K <opam.qweiop@gmail.com>
2022-12-01 21:41:10 +0700

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/opam22/scheduler
+
+go 1.18
---
+++ scheduler.go
@@ -0,0 +1,81 @@
+package scheduler
+
+import (
+	"math"
+	"time"
+)
+
+type Unit int
+
+const (
+	Seconds Unit = iota
+	Minutes
+	Hour
+	Day
+)
+
+type Handler struct {
+	Log string
+	Job []Job
+}
+
+type Job struct {
+	Name         string
+	Every        int32
+	Unit         Unit
+	Task         func()
+	RegisteredAt time.Time
+}
+
+func New(log string) *Handler {
+	return &Handler{
+		Log: log,
+	}
+}
+
+func (h *Handler) AddJob(job Job) {
+	j := Job{
+		Name:         job.Name,
+		Every:        job.Every,
+		Unit:         job.Unit,
+		Task:         job.Task,
+		RegisteredAt: time.Now(),
+	}
+
+	h.Job = append(h.Job, j)
+}
+
+func (h *Handler) Start() {
+	for range time.Tick(time.Second * 1) {
+		for _, j := range h.Job {
+			h.Run(j)
+		}
+	}
+}
+
+func (h *Handler) Run(job Job) {
+	if ShouldRun(job) {
+		job.Task()
+	}
+}
+
+func ShouldRun(job Job) bool {
+	currentTime := time.Now()
+	delta := currentTime.Sub(job.RegisteredAt)
+
+	if job.Unit == Seconds {
+		if int32(math.Ceil(delta.Seconds()))%job.Every == 0 {
+			return true
+		}
+	} else if job.Unit == Minutes {
+		if int32(math.Ceil(delta.Seconds()))%(job.Every*60) == 0 {
+			return true
+		}
+	} else if job.Unit == Hour {
+		if int32(math.Ceil(delta.Seconds()))%(job.Every*3600) == 0 {
+			return true
+		}
+	}
+
+	return false
+}
